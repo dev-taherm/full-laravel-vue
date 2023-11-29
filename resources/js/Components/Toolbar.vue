2591a7ecb618ab0f485865b46196a25dba744911
dev-taherm <tahermahram0@gmail.com>
2023-11-28 16:13:41 -1000

    changing the toolbar

--- resources/js/Components/Toolbar.vue
+++ resources/js/Components/Toolbar.vue
@@ -1,76 +1,239 @@
 <script setup lang="ts">
 import { Link } from "@inertiajs/vue3";
-import { useTheme } from "vuetify";
-import { ref } from "vue";
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+} from "@headlessui/vue";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
 
-const items = ref([
-    { title: "Click Me" },
-    { title: "Click Me" },
-    { title: "Click Me" },
-    { title: "Click Me 2" },
-]);
-
-const theme = useTheme();
-
-function toggleTheme() {
-    theme.global.name.value = theme.global.current.value.dark
-        ? "light"
-        : "dark";
-}
+const user = {
+    name: "Tom Cook",
+    email: "tom@example.com",
+    imageUrl:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
+const navigation = [
+    { name: "Home", href: route("home"), current: true },
+    { name: "Team", href: "#", current: false },
+    { name: "Projects", href: "#", current: false },
+    { name: "Calendar", href: "#", current: false },
+    { name: "Reports", href: "#", current: false },
+];
+const userNavigation = [
+    { name: "Your Profile", href: "#" },
+    { name: "Settings", href: "#" },
+    { name: "Sign out", href: route("logout") },
+];
 </script>
 
 <template>
-    <v-toolbar density="compact" :elevation="6">
-        <v-toolbar-title class="text-secondary">Anqas</v-toolbar-title>
+    <!--
+    This example requires updating your template:
 
-        <v-spacer></v-spacer>
-      
-        <template v-if="$page.props.auth.user">
-            <v-btn icon>
-                <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-
-            <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-            </v-btn>
-           
-                <v-menu>
-                    <template v-slot:activator="{ props }">
-                        <v-btn icon="mdi-dots-vertical" v-bind="props"></v-btn>
-                    </template>
-
-                    <v-list>
-                        <v-list-item>
-                            <v-list-item-title>
-                             <v-btn prepend-icon="mdi-logout" variant="text">
-                                  <Link
-                                    :href="route('logout')"
-                                    method="post"
-                                    as="button"
-                                    >Logout</Link
+    ```
+    <html class="h-full bg-gray-100">
+    <body class="h-full">
+    ```
+  -->
+    <div class="min-h-full">
+        <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="flex h-16 items-center justify-between">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0">
+                            <img
+                                class="h-8 w-8"
+                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                                alt="Your Company"
+                            />
+                        </div>
+                        <div class="hidden md:block">
+                            <div class="ml-5 flex items-baseline space-x-4">
+                                <Link
+                                    v-for="item in navigation"
+                                    :key="item.name"
+                                    :href="item.href"
+                                    :class="[
+                                        item.current
+                                            ? 'bg-gray-900 text-white'
+                                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        'rounded-md px-3 py-2 text-sm font-medium',
+                                    ]"
+                                    :aria-current="
+                                        item.current ? 'page' : undefined
+                                    "
+                                    >{{ item.name }}</Link
                                 >
-                              
-                              </v-btn>
-                                
-                            </v-list-item-title>
-                            <v-list-item-title>
-                           <v-btn
-           prepend-icon="mdi-theme-light-dark"
-            @click="toggleTheme"
-            variant="text"
-        > Theme</v-btn>
-                                
-                            </v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-          
-        </template>
+                            </div>
+                        </div>
+                    </div>
 
-        <template v-else>
-            <v-btn variant="tonal" size="small" class="mr-5 bg-secondary">
-                <Link :href="route('login')">Log in</Link>
-            </v-btn>
-        </template>
-    </v-toolbar>
+                    <div class="hidden md:block">
+                        <div class="ml-4 flex items-center md:ml-6">
+                            <template v-if="$page.props.auth.user">
+                                <button
+                                    type="button"
+                                    class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                >
+                                    <span class="absolute -inset-1.5" />
+                                    <span class="sr-only"
+                                        >View notifications</span
+                                    >
+                                    <BellIcon
+                                        class="h-6 w-6"
+                                        aria-hidden="true"
+                                    />
+                                </button>
+
+                                <!-- Profile dropdown -->
+                                <Menu as="div" class="relative ml-3">
+                                    <div>
+                                        <MenuButton
+                                            class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                        >
+                                            <span class="absolute -inset-1.5" />
+                                            <span class="sr-only"
+                                                >Open user menu</span
+                                            >
+                                            <img
+                                                class="h-8 w-8 rounded-full"
+                                                :src="user.imageUrl"
+                                                alt=""
+                                            />
+                                        </MenuButton>
+                                    </div>
+                                    <transition
+                                        enter-active-class="transition ease-out duration-100"
+                                        enter-from-class="transform opacity-0 scale-95"
+                                        enter-to-class="transform opacity-100 scale-100"
+                                        leave-active-class="transition ease-in duration-75"
+                                        leave-from-class="transform opacity-100 scale-100"
+                                        leave-to-class="transform opacity-0 scale-95"
+                                    >
+                                        <MenuItems
+                                            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                        >
+                                            <MenuItem
+                                                v-for="item in userNavigation"
+                                                :key="item.name"
+                                                v-slot="{ active }"
+                                            >
+                                                <Link
+                                                    :href="item.href"
+                                                     method="post"
+                                                     as="button"
+                                                    :class="[
+                                                        active
+                                                            ? 'bg-gray-100'
+                                                            : '',
+                                                        'block px-4 py-2 text-sm text-gray-700',
+                                                    ]"
+                                                    >{{ item.name }}
+                                                </Link>
+                                            </MenuItem>
+                                        </MenuItems>
+                                            
+                                           
+                                      
+                                    </transition>
+                                </Menu>
+                            </template>
+                            <template v-else>
+                                <button
+                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                >
+                                 <Link :href="route('login')" >Login</Link>
+                                </button>
+                            </template>
+                        </div>
+                    </div>
+
+                    <div class="-mr-2 flex md:hidden">
+                        <!-- Mobile menu button -->
+                        <DisclosureButton
+                            class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        >
+                            <span class="absolute -inset-0.5" />
+                            <span class="sr-only">Open main menu</span>
+                            <Bars3Icon
+                                v-if="!open"
+                                class="block h-6 w-6"
+                                aria-hidden="true"
+                            />
+                            <XMarkIcon
+                                v-else
+                                class="block h-6 w-6"
+                                aria-hidden="true"
+                            />
+                        </DisclosureButton>
+                    </div>
+                </div>
+            </div>
+
+            <DisclosurePanel class="md:hidden">
+                <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                    <DisclosureButton
+                        v-for="item in navigation"
+                        :key="item.name"
+                        as="a"
+                        :href="item.href"
+                        :class="[
+                            item.current
+                                ? 'bg-gray-900 text-white'
+                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'block rounded-md px-3 py-2 text-base font-medium',
+                        ]"
+                        :aria-current="item.current ? 'page' : undefined"
+                        >{{ item.name }}</DisclosureButton
+                    >
+                </div>
+                <div class="border-t border-gray-700 pb-3 pt-4">
+                    <div class="flex items-center px-5">
+                        <div class="flex-shrink-0">
+                            <img
+                                class="h-10 w-10 rounded-full"
+                                :src="user.imageUrl"
+                                alt=""
+                            />
+                        </div>
+                        <div class="ml-3">
+                            <div
+                                class="text-base font-medium leading-none text-white"
+                            >
+                                {{ $page.props.auth.user.name }}
+                            </div>
+                            <div
+                                class="text-sm font-medium leading-none text-gray-400"
+                            >
+                                {{ $page.props.auth.user.email }}
+                            </div>
+                        </div>
+                        <button
+                            type="button"
+                            class="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        >
+                            <span class="absolute -inset-1.5" />
+                            <span class="sr-only">View notifications</span>
+                            <BellIcon class="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+                    <div class="mt-3 space-y-1 px-2">
+                        <DisclosureButton
+                            v-for="item in userNavigation"
+                            :key="item.name"
+                            as="a"
+                            :href="item.href"
+                            class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                            >{{ item.name }}</DisclosureButton
+                        >
+                    </div>
+                </div>
+            </DisclosurePanel>
+        </Disclosure>
+    </div>
 </template>
---
+++ resources/js/Components/toolbar2.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-toolbar density="compact" :elevation="6">
+        <v-toolbar-title class="text-secondary">Anqas</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <template v-if="$page.props.auth.user">
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+                <v-menu>
+                    <template v-slot:activator="{ props }">
+                        <v-btn icon="mdi-dots-vertical" v-bind="props"></v-btn>
+                    </template>
+
+                    <v-list>
+                        <v-list-item>
+                            <v-list-item-title>
+                             <v-btn prepend-icon="mdi-logout" variant="text">
+                                  <Link
+                                    :href="route('logout')"
+                                    method="post"
+                                    as="button"
+                                    >Logout</Link
+                                >
+
+                              </v-btn>
+
+                            </v-list-item-title>
+                            <v-list-item-title>
+                           <v-btn
+           prepend-icon="mdi-theme-light-dark"
+            @click="toggleTheme"
+            variant="text"
+        > Theme</v-btn>
+
+                            </v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+
+        </template>
+
+        <template v-else>
+            <v-btn variant="tonal" size="small" class="mr-5 bg-secondary">
+                <Link :href="route('login')">Log in</Link>
+            </v-btn>
+        </template>
+    </v-toolbar>
+</template>
